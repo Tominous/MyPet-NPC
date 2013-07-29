00338ab6c54ac209e7faceb2bc4575c9a4ad94db
xXKeyleXx <keyle@t-online.de>
2013-07-29 12:31:04 +0200

    changed permissions for the mypet-storage trait

--- src/main/java/de/Keyle/MyPet/Npc/npc/traits/MyPetStorageTrait.java
+++ src/main/java/de/Keyle/MyPet/Npc/npc/traits/MyPetStorageTrait.java
@@ -57,7 +57,7 @@ public class MyPetStorageTrait extends Trait
 
         final Player player = npcEvent.getClicker();
 
-        if (!Permissions.has(player, "MyPet.Npc.interact"))
+        if (!Permissions.has(player, "MyPet.npc.storage.interact"))
         {
             player.sendMessage(Locales.getString("Message.No.Allowed", player));
             return;
@@ -81,7 +81,7 @@ public class MyPetStorageTrait extends Trait
                 int maxPetCount = 0;
                 for (int i = 54 ; i >= 9 ; i -= 9)
                 {
-                    if (Permissions.has(player, "MyPet.Npc.max." + i))
+                    if (Permissions.has(player, "MyPet.npc.storage.max." + i))
                     {
                         maxPetCount = i;
                         break;
