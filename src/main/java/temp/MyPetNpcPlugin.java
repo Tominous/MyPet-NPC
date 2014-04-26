1d0b7ce72f96c21424e51881e9896ae7d2b5f465
xXKeyleXx <keyle@t-online.de>
2014-04-26 11:34:41 +0200

    refactored packages (part2)

--- src/main/java/temp/MyPetNpcPlugin.java
+++ src/main/java/temp/MyPetNpcPlugin.java
@@ -18,7 +18,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-package de.keyle.mypet.npc;
+package temp;
 
 import de.Keyle.MyPet.MyPetPlugin;
 import de.Keyle.MyPet.util.MyPetVersion;
